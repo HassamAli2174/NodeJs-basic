e39c7a127ee94126069f2c9fd0cf4755bb4a892c
HassamAli2174 <hassamarshad021@gmail.com>
2025-05-20 17:07:37 +0500

    Add POST request handling to save messages in a file and redirecting it

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const { write } = require('fs');
 const http = require('http');
-const { url } = require('inspector');
+const fs = require('fs'); //filesystem for creating a new file
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers)
     const url = req.url
+    const method = req.method
     if ( url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
         res.write('</html>')
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt','Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
