3ff8cfb7814fee83216522f5cd493bb562bfb11e
HassamAli2174 <hassamarshad021@gmail.com>
2025-06-21 15:01:48 +0500

    Update middleware logging and add route for "Add Product" page in Express app

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import express from 'express';
 const app = express();
 
 app.use((req,res,next)=>{
-    console.log('In the middleware');
+    console.log('This Always Runs');
     next(); //allows the request to continue to the next middleware in line 
 })
-app.use((req,res,next)=>{
+
+app.use('/add-product',(req,res,next)=>{
+    console.log('The middleware');
+    res.send('<h1>The "Add Product" Page</h1>');
+})
+
+app.use('/',(req,res,next)=>{
     console.log('In the next middleware');
     res.send('<h1>Hello from express</h1>');
-    next();
 })
 
 app.listen(3000);
