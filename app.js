014e00d5f28dbedb20abbcd09bbcde7a14f77c33
HassamAli2174 <hassamarshad021@gmail.com>
2025-06-21 14:33:16 +0500

    Update middleware to log requests and send HTML response from Express server

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const app = express();
 
 app.use((req,res,next)=>{
     console.log('In the middleware');
-    next();
+    next(); //allows the request to continue to the next middleware in line 
 })
 app.use((req,res,next)=>{
     console.log('In the next middleware');
+    res.send('<h1>Hello from express</h1>');
     next();
 })
 
